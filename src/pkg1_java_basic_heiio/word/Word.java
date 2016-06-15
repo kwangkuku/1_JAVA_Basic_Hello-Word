637b7da771ca5c350e6b098673f5ea826b240f03
KWANGkuku <kwangkuku@gmail.com>
2016-06-16 02:54:04 +0700

    Hello Word

---
+++ src/pkg1_java_basic_heiio/word/Word.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1_java_basic_heiio.word;
+
+/**
+ *
+ * @author Kwanrudee
+ */
+public class Word {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
